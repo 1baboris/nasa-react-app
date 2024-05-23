bcee80ce35b7721b15ae2ba0c0d56c90f8530c47
baboris <boris.benachour@gmail.com>
2024-05-23 17:51:43 +0200

    CheckPoint

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,22 @@
+export default function SideBar(props) {
+  const { handleToggleModal } = props;
+
+  return (
+    <div className="sidebar">
+      <div onClick={handleToggleModal} className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            dignissimos ipsum delectus necessitatibus deserunt dolorem.
+          </p>
+        </div>
+        <button onClick={handleToggleModal}>
+          <i className="fa-solid fa-right-long"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
