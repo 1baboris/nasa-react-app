84ad3eeee7e5912668a50be6088e0fb51afc1f7d
baboris <boris.benachour@gmail.com>
2024-05-23 20:51:05 +0200

    fin du tuto

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,56 @@ import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 
 function App() {
-  const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  console.log(NASA_KEY);
 
   function handleToggleModal() {
     setShowModal(!showModal);
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched from cache today");
+        return;
+      }
+      localStorage.clear();
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.parse(apiData));
+        setData(apiData);
+        console.log("Fetched from API today");
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
 
   return (
     <>
-      <Main />
-      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
-      <Footer handleToggleModal={handleToggleModal} />
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,14 @@
 export default function SideBar(props) {
-  const { handleToggleModal } = props;
+  const { handleToggleModal, data } = props;
 
   return (
     <div className="sidebar">
       <div onClick={handleToggleModal} className="bgOverlay"></div>
       <div className="sidebarContents">
-        <h2>The Brutal Martian Landscape</h2>
-        <div>
-          <p>Description</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-            dignissimos ipsum delectus necessitatibus deserunt dolorem.
-          </p>
+        <h2>{data?.title}</h2>
+        <div className="descriptionContainer">
+          <p className="descriptionTitle">{data?.date}</p>
+          <p>{data?.explanation}</p>
         </div>
         <button onClick={handleToggleModal}>
           <i className="fa-solid fa-right-long"></i>
